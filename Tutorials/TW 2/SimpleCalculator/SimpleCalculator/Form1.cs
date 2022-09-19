582ca045066271db8f9ac1b31590dd032f1a62f3
vasiliyk <vasiliyk@synel.co.il>
2022-09-19 19:30:12 +0500

    TW 2

---
+++ Tutorials/TW 2/SimpleCalculator/SimpleCalculator/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            var number1 = Convert.ToDouble(tbxNumber1.Text);
+            var number2 = Convert.ToDouble(tbxNumber2.Text);
+            var result = number1 + number2;
+
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            var number1 = Convert.ToDouble(tbxNumber1.Text);
+            var number2 = Convert.ToDouble(tbxNumber2.Text);
+            var result = number1 - number2;
+
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            var number1 = Convert.ToDouble(tbxNumber1.Text);
+            var number2 = Convert.ToDouble(tbxNumber2.Text);
+            var result = number1 / number2;
+
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            var number1 = Convert.ToDouble(tbxNumber1.Text);
+            var number2 = Convert.ToDouble(tbxNumber2.Text);
+            var result = number1 * number2;
+
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnReminder_Click(object sender, EventArgs e)
+        {
+            var number1 = Convert.ToDouble(tbxNumber1.Text);
+            var number2 = Convert.ToDouble(tbxNumber2.Text);
+            var result = number1 % number2;
+
+            lblResult.Text = result.ToString();
+        }
+
+        private void btnExp_Click(object sender, EventArgs e)
+        {
+            var number1 = Convert.ToDouble(tbxNumber1.Text);
+            var number2 = Convert.ToDouble(tbxNumber2.Text);
+            var result = Math.Pow(number1, number2);
+
+            lblResult.Text = result.ToString();
+        }
+    }
+}
---
+++ Tutorials/TW 2/StrangeLanguage/StrangeLanguage/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace StrangeLanguage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            //translation rules
+            //wo|rk => rk|wo
+            //stu|dent => dent|stu
+
+            var input = tbxInput.Text;
+            var length = input.Length;
+
+            //NOTE: in C# dividing integer over integer results in another integer
+            //thus 3 / 2 is equal to 1, not to 1.5 or 2
+            //use 3.0 / 2 or 3 / 2.0 to get 1.5
+            //uncomment the following lines to explore
+            //MessageBox.Show((3 / 2).ToString()); //shows 1
+            //MessageBox.Show((3.0 / 2).ToString()); //shows 1.5
+            //MessageBox.Show((3 / 2.0).ToString()); //shows 1.5
+
+            //back to topic
+            var lengthOfFirstPart = length / 2;
+            var firstPart = input.Substring(0, lengthOfFirstPart);
+            var secondPart = input.Substring(lengthOfFirstPart);
+            var result = secondPart + firstPart;
+            MessageBox.Show(result);
+        }
+    }
+}
