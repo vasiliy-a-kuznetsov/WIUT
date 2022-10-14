c7149aa62a1fe8aa9f0061a9c1f8b1859306d573
Vasiliy Kuznetsov <vasiliy.a.kuznetsov@gmail.com>
2022-10-14 17:10:46 +0500

    TW 6

---
+++ Tutorials/TW 6/Numerology/Numerology/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Numerology
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.tbxDob = new System.Windows.Forms.TextBox();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblType = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(169, 21);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // tbxDob
+            // 
+            this.tbxDob.Location = new System.Drawing.Point(39, 21);
+            this.tbxDob.Name = "tbxDob";
+            this.tbxDob.Size = new System.Drawing.Size(100, 20);
+            this.tbxDob.TabIndex = 6;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(199, 54);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(35, 13);
+            this.lblNumber.TabIndex = 5;
+            this.lblNumber.Text = "label2";
+            // 
+            // lblType
+            // 
+            this.lblType.AutoSize = true;
+            this.lblType.Location = new System.Drawing.Point(45, 54);
+            this.lblType.Name = "lblType";
+            this.lblType.Size = new System.Drawing.Size(35, 13);
+            this.lblType.TabIndex = 4;
+            this.lblType.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 74);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.tbxDob);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.lblType);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox tbxDob;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblType;
+    }
+}
+
---
+++ Tutorials/TW 6/Numerology/Numerology/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Numerology
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var input = tbxDob.Text;
+            DateTime result;
+            //validate
+            if (DateTime.TryParse(input, out result))
+            {
+                DateTime dob = Convert.ToDateTime(input);
+                int number = (dob.Year + dob.Month + dob.Day);
+
+                //sum up digits
+                while (number > 9)
+                {
+                    int sumOfDigits = 0;
+                    while (number > 0)
+                    {
+                        int lastDigit = number % 10;
+                        sumOfDigits += lastDigit;
+
+                        //remove last digit from the number
+                        number = number / 10;
+                    }
+                    //reset number to the current sum
+                    number = sumOfDigits;
+                }
+
+                //by now we have a correct number
+                string personType;
+                switch (number)
+                {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        personType = "vampire";
+                        break;
+                    case 5:
+                    case 6:
+                    case 7:
+                        personType = "neutral";
+                        break;
+                    default:
+                        personType = "angel";
+                        break;
+                }
+                
+                //show the result
+                lblNumber.Text = Convert.ToString(number);
+                lblType.Text = personType;
+            }
+
+            else
+                //invalid input
+                MessageBox.Show("Please enter correct date");
+        }
+    }
+}
