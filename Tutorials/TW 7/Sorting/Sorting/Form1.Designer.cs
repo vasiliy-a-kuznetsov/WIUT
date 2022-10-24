218a0604e5924b615e925be718ae6fb3422d649b
Vasiliy Kuznetsov <vasiliy.a.kuznetsov@gmail.com>
2022-10-24 16:35:20 +0500

    TW 7

---
+++ Tutorials/TW 7/Sorting/Sorting/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Sorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(12, 12);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(286, 20);
+            this.tbxInput.TabIndex = 0;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(304, 10);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 1;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 51);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.tbxInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
