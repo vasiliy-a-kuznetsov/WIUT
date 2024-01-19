5111f8da134392411cb0f331fa54e4fbd5702123
Vasiliy Kuznetsov <vasiliy.a.kuznetsov@gmail.com>
2024-01-19 17:54:34 +0500

    TW 15

---
+++ Seminars/TW 15/WIUT/WIUT.DAL/ApplicantManager.cs
@@ -0,0 +1,202 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace WIUT.DAL
+{
+    public class ApplicantManager
+    {
+        public void Create(Applicant a)
+        {
+            using var connection = new SQLiteConnection("");
+            try
+            {
+                var sql = $@"
+INSERT INTO Applicant
+           (Name
+           ,Surname
+           ,Address
+           ,DoB
+           ,IsMarried
+           ,PassportNo
+           ,CourseId)
+     VALUES
+           ('{a.Name}'
+           ,'{a.Surname}'
+           ,'{a.Address}'
+           ,{a.DoB.Ticks}
+           ,{Convert.ToInt32(a.IsMarried)}
+           ,'{a.PassportNo}'
+           ,{a.Course.Id})";
+
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                    connection.Close();
+            }
+        }
+
+        public void Update(Applicant a)
+        {
+            using var connection = new SQLiteConnection("");
+            try
+            {
+                var sql = $@"
+UPDATE Applicant 
+   SET Name = '{a.Name}'
+      ,Surname = '{a.Surname}'
+      ,Address = '{a.Address}'
+      ,DoB = {a.DoB.Ticks}
+      ,IsMarried = {Convert.ToInt32(a.IsMarried)}
+      ,PassportNo = '{a.PassportNo}'
+      ,CourseId = {a.Course.Id}
+WHERE Id = {a.Id}";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using var connection = new SQLiteConnection("");
+            try
+            {
+                var sql = $"DELETE FROM Applicant WHERE Id = {id}";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public Applicant GetById(int id)
+        {
+            using var connection = new SQLiteConnection("");
+            try
+            {
+                var sql = $@"
+SELECT Id
+      ,Name
+      ,Surname
+      ,Address
+      ,DoB
+      ,IsMarried
+      ,PassportNo
+      ,CourseId
+FROM Applicant
+WHERE ID = {id}";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                using var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    return new Applicant
+                    {
+                        Id = Convert.ToInt32(reader.GetValue(0)),
+                        Name = reader.GetValue(1).ToString(),
+                        Surname = reader.GetValue(2).ToString(),
+                        Address = reader.GetValue(3).ToString(),
+                        DoB = new DateTime(Convert.ToInt64(reader.GetValue(4))),
+                        IsMarried = Convert.ToBoolean(Convert.ToInt32(reader.GetValue(5))),
+                        PassportNo = reader.GetValue(6).ToString(),
+                        Course = new CourseManager().GetById(Convert.ToInt32(reader.GetValue(7)))
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+
+            // if we are here - something went wrong
+            return null;
+        }
+
+        public List<Applicant> GetAll()
+        {
+            using var connection = new SQLiteConnection("");
+            var result = new List<Applicant>();
+            try
+            {
+                var sql = @"
+SELECT Id
+      ,Name
+      ,Surname
+      ,Address
+      ,DoB
+      ,IsMarried
+      ,PassportNo
+      ,CourseId
+FROM Applicant";
+                var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var a = new Applicant
+                    {
+                        Id = Convert.ToInt32(reader.GetValue(0)),
+                        Name = reader.GetValue(1).ToString(),
+                        Surname = reader.GetValue(2).ToString(),
+                        Address = reader.GetValue(3).ToString(),
+                        DoB = new DateTime(Convert.ToInt64(reader.GetValue(4))),
+                        IsMarried = Convert.ToBoolean(Convert.ToInt32(reader.GetValue(5))),
+                        PassportNo = reader.GetValue(6).ToString(),
+                        Course = new CourseManager().GetById(Convert.ToInt32(reader.GetValue(7)))
+                    };
+                    result.Add(a);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Seminars/TW 15/WIUT/WIUT.DAL/CourseManager.cs
@@ -0,0 +1,147 @@
+﻿using System.Data;
+using System.Data.SQLite;
+
+namespace WIUT.DAL
+{
+    public class CourseManager
+    {
+        public void Create(Course c)
+        {
+            using var connection = new SQLiteConnection();
+            try
+            {
+                var sql = $"INSERT INTO Course (Name) VALUES ('{c.Name}')";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                    connection.Close();
+            }
+        }
+
+        public void Update(Course c)
+        {
+            using var connection = new SQLiteConnection("");
+            try
+            {
+                var sql = $"UPDATE Course SET Name = '{c.Name}' WHERE Id = {c.Id}";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using var connection = new SQLiteConnection("");
+            try
+            {
+                var sql = $"DELETE FROM Course WHERE Id = {id}";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+            }
+        }
+
+        public Course GetById(int id)
+        {
+            using var connection = new SQLiteConnection("");
+            try
+            {
+                var sql = $"SELECT Id, Name FROM Course WHERE Id = {id}";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                using var reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    var c = new Course
+                    {
+                        Id = Convert.ToInt32(reader.GetValue(0)),
+                        Name = Convert.ToString(reader.GetValue(1))
+                    };
+                    return c;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                {
+                    connection.Close();
+                }
+
+            }
+
+            // if we are here - something went wrong
+            return null;
+        }
+
+
+        public List<Course> GetAll()
+        {
+            using var connection = new SQLiteConnection();
+            var result = new List<Course>();
+            try
+            {
+                var sql = "SELECT Id, Name FROM Course";
+                using var command = new SQLiteCommand(sql, connection);
+                connection.Open();
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var c = new Course
+                    {
+                        Id = Convert.ToInt32(reader.GetValue(0)),
+                        Name = Convert.ToString(reader.GetValue(1))
+                    };
+                    result.Add(c);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (connection.State != ConnectionState.Closed)
+                    connection.Close();
+            }
+
+            return result;
+        }
+
+    }
+}
