37e94905438526a233a2052b09b196726f88b426
Vasiliy Kuznetsov <vasiliy.a.kuznetsov@gmail.com>
2024-01-26 22:12:46 +0500

    TW 16

---
+++ Seminars/TW 16/WIUT/WIUT/ParentForm.cs
@@ -0,0 +1,20 @@
+﻿namespace WIUT
+{
+    public partial class ParentForm : Form
+    {
+        public ParentForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new AboutForm().ShowDialog();
+        }
+    }
+}
