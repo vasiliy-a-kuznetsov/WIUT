5273601d11f87087b299029a3cf0f30a4c8b8cec
Vasiliy Kuznetsov <vasiliy.a.kuznetsov@gmail.com>
2024-09-21 10:32:41 +0500

    TW 2

---
+++ Seminars/TW 2/Exchanger/Exchanger/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exchanger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var amount = Convert.ToDouble(tbxAmount.Text);
+            var rate = Convert.ToDouble(tbxRate.Text);
+
+            var result = amount / rate;
+            
+            //round the result to have two decimal places
+            result = Math.Round(result, 2);
+
+            //round the result to the nearest hundred
+            result = Math.Round(result / 100) * 100;
+
+            MessageBox.Show(result.ToString());
+        }
+    }
+}
---
+++ Seminars/TW 2/InterestCalculator/InterestCalculator/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InterestCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            var name = tbxName.Text;
+            var amount = nudAmount.Value;
+            var rate = nudRate.Value;
+
+            var interest = amount * rate / 100;
+
+            var result = amount + interest;
+
+            string message = "";
+            message += "Name: " + name;
+            message += "\n";
+            message += "Amount: " + amount;
+            message += "\n";
+            message += "Rate: " + rate;
+            message += "\n";
+            message += "Balance: " + result;
+            MessageBox.Show(message);
+        }
+    }
+}
