edf7e22dfb84569abbd19ee9904661aa2469acc2
Vasiliy Kuznetsov <vasiliy.a.kuznetsov@gmail.com>
2025-01-24 16:27:11 +0500

    TW 15

---
+++ Seminars/TW 15/WIUT/WIUT.DAL/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace WIUT.DAL
+{
+
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+       
+        public Course()
+        {
+
+        }
+
+        public Course(string name)
+        {
+            Name = name;
+        }
+    }
+}
